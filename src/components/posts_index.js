a15204739328feaa4ee82029494e7c211289d971
BlackMG27 <oike279@gmail.com>
2016-05-14 12:46:23 -0500

    working on post view css

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -15,8 +15,20 @@ class PostsIndex extends Component {
 		  schools.push(
 		    <li className="list-group-item" key={key}>
 		      <Link to={"posts/" + key}>
-		        <strong>{post.school_name}</strong>
+		        <strong className="school-name">{post.school_name}</strong>
 		      </Link>
+          <li>{post.school_info}</li>
+          <div className="days">
+          <span className="days-since">23</span>
+          <span className="days-since-text">Days Since Order</span>
+          </div>
+          <ul>
+          <li className="list-items">Five PCs</li>
+          <li className="list-items">Seven Laptops</li>
+          <li className="list-items">Five Monitors</li>
+          <li className="list-items">6 Keyboards</li>
+          </ul>
+
 		    </li>
 		  );
   	}
